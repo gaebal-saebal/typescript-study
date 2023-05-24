2aefe0375bac1ce4010c316ae921a7454755e68e
js-ha <jshaha0911@gmail.com>
2023-05-24 15:39:52 +0900

    js-ha / 230524(2)

--- js-ha/230524/index.js
+++ js-ha/230524/index.js
@@ -28,3 +28,79 @@ var garen = new 기계('strike', 'courage');
 기계.prototype.name = 'ha'; // 오브젝트에 자료 추가했음
 // 부모의 유전자에 있으면 자식이 가져다가 사용할 수 있음
 // 유전자에 추가해주면 부모만 가지고 있음
+//
+var Person = /** @class */ (function () {
+    function Person(a) {
+        // Person에서 생성되는 자료형은 전부 오브젝트니까 타입지정 필요없음
+        this.name = a;
+    }
+    Person.prototype.함수 = function (b) {
+        console.log('hello' + b);
+    };
+    return Person;
+}());
+var 사람1 = new Person('ha');
+var 사람2 = new Person('shin');
+사람1.함수('lalala');
+//
+var Car = /** @class */ (function () {
+    function Car(a, b) {
+        this.model = a;
+        this.price = b;
+    }
+    Car.prototype.tax = function (b) {
+        return b / 10;
+    };
+    return Car;
+}());
+var test = new Car('몰라', 3000);
+test.tax(3000);
+//
+var Word = /** @class */ (function () {
+    function Word() {
+        var params = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            params[_i] = arguments[_i];
+        }
+        var arr1 = [];
+        var arr2 = [];
+        params.forEach(function (a) {
+            if (typeof a === 'number') {
+                arr1.push(a);
+            }
+            else {
+                arr2.push(a);
+            }
+        });
+        this.num = arr1;
+        this.str = arr2;
+    }
+    return Word;
+}());
+var obj = new Word('kim', 3, 5, 'park');
+console.log(obj.num); //[3,5]
+console.log(obj.str); //['kim', 'park']
+//
+var 네모 = {
+    color: 'orange',
+    width: 200,
+};
+// interface Teacher {
+//   name: string;
+//   age: number;
+// }
+var 학생 = { name: 'kim' };
+var 선생님 = { name: 'ha', age: 20 };
+var 상품 = { brand: 'Samsung', serialNumber: 1360, model: ['TV', 'phone'] };
+var 장바구니 = [
+    { product: '청소기', price: 7000 },
+    { product: '삼다수', price: 800 },
+];
+var aaa = {
+    plus: function (a, b) {
+        return a + b;
+    },
+    minus: function (a, b) {
+        return a - b;
+    },
+};
--- js-ha/230524/index.ts
+++ js-ha/230524/index.ts
@@ -33,3 +33,132 @@ const garen = new 기계('strike', 'courage');
 기계.prototype.name = 'ha'; // 오브젝트에 자료 추가했음
 // 부모의 유전자에 있으면 자식이 가져다가 사용할 수 있음
 // 유전자에 추가해주면 부모만 가지고 있음
+
+//
+class Person {
+  name;
+  constructor(a: string) {
+    // Person에서 생성되는 자료형은 전부 오브젝트니까 타입지정 필요없음
+    this.name = a;
+  }
+  함수(b: string) {
+    console.log('hello' + b);
+  }
+}
+
+let 사람1 = new Person('ha');
+let 사람2 = new Person('shin');
+
+사람1.함수('lalala');
+
+//
+class Car {
+  model;
+  price;
+  constructor(a: string, b: number) {
+    this.model = a;
+    this.price = b;
+  }
+  tax(b: number) {
+    return b / 10;
+  }
+}
+
+let test = new Car('몰라', 3000);
+test.tax(3000);
+
+//
+class Word {
+  num;
+  str;
+  constructor(...params: (string | number)[]) {
+    let arr1: number[] = [];
+    let arr2: string[] = [];
+    params.forEach((a) => {
+      if (typeof a === 'number') {
+        arr1.push(a);
+      } else {
+        arr2.push(a);
+      }
+    });
+    this.num = arr1;
+    this.str = arr2;
+  }
+}
+
+let obj = new Word('kim', 3, 5, 'park');
+console.log(obj.num); //[3,5]
+console.log(obj.str); //['kim', 'park']
+
+//
+// type Square = { color: string; width: number };
+interface Square {
+  color: string;
+  width: number;
+}
+//
+
+let 네모: Square = {
+  color: 'orange',
+  width: 200,
+};
+
+//
+interface Student {
+  name: string;
+}
+
+// interface Teacher {
+//   name: string;
+//   age: number;
+// }
+
+let 학생: Student = { name: 'kim' };
+let 선생님: Teacher = { name: 'ha', age: 20 };
+
+// interface 장점 : extends로 복사가능
+interface Teacher extends Student {
+  age: number;
+}
+
+type Animal = { name: string };
+type Cat = { age: number } & Animal;
+
+// 타입은 & , 중복선언 허용하지 않음
+// interface는 extends, 중복선언 허용(외부라이브러리에서 자주 사용 =>  덮어쓰면 되니까 편리해서)
+
+interface Product {
+  brand: string;
+  serialNumber: number;
+  model: string[];
+}
+
+let 상품: Product = { brand: 'Samsung', serialNumber: 1360, model: ['TV', 'phone'] };
+
+interface Cart {
+  product: string;
+  price: number;
+}
+
+interface Card extends Cart {
+  card: boolean;
+}
+
+let 장바구니: Cart[] = [
+  { product: '청소기', price: 7000 },
+  { product: '삼다수', price: 800 },
+];
+
+interface ObjType {
+  plus: (a: number, b: number) => number;
+  minus: (a: number, b: number) => number;
+}
+
+let aaa: ObjType = {
+  plus(a, b) {
+    return a + b;
+  },
+  minus(a, b) {
+    return a - b;
+  },
+};
